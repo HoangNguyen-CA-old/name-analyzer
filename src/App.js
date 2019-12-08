6ca17e37b6641a7498381c356bcf1677bcd457b1
Hoang Nguyen <hoang.viet.nguyen@outlook.com>
2019-12-08 18:04:35 -0500

    added basic layout and api fetching

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import './App.scss';
+import Form from './components/form';
 
 function App() {
+  let loading = useState(true);
+
+  let getAge = name => {
+    var data = null;
+
+    var xhr = new XMLHttpRequest();
+    //xhr.withCredentials = true;
+
+    xhr.addEventListener('readystatechange', function() {
+      if (this.readyState === this.DONE) {
+        console.log(this.responseText);
+      }
+    });
+
+    xhr.open('GET', `https://api.agify.io?name=${name}`);
+
+    xhr.send(data);
+  };
+
+  useEffect(() => {}, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Form />
+    </>
   );
 }
 
---
+++ src/components/form.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Button, Container, Row, Col } from 'react-bootstrap';
+import '../styles/form.scss';
+
+export default function form() {
+  return (
+    <div class='text-center' id='form_wrapper'>
+      <input></input>
+      <br />
+      <Button>See Age</Button>
+    </div>
+  );
+}
